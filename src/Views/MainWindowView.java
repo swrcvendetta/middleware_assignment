7f34c8b9ee5861cd27f5affce858e7474a12027a
swrcpatch <swrcpatch@gmail.com>
2024-02-12 15:44:33 +0100

    Added TabPages + Interface to MainWindowView

--- src/Views/MainWindowView.java
+++ src/Views/MainWindowView.java
@@ -1,22 +1,35 @@
 package Views;
 
 import Events.PropertyChangedEventArgs;
+import Interfaces.INotifyPropertyChanged;
+import Interfaces.ITabPageView;
 import ViewModels.MainWindowViewModel;
 
 import javax.swing.*;
 
 public class MainWindowView extends ViewBase {
     public MainWindowViewModel model;
+    private JPanel basepanel;
+    private JTabbedPane tabcontrol;
+
     public MainWindowView() {
         super();
         this.model = new MainWindowViewModel(this);
-        this.window.setTitle("Test");
-        this.window.setSize(600, 400);
-        this.window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.window.setResizable(false);
-        this.window.setLocationRelativeTo(null);
-    }
+        this.setContentPane(this.basepanel);
+        this.setTitle("MainWindowView");
+        this.setSize(600, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
 
+        ITabPageView loginPage = new LoginTabPageView();
+        ITabPageView chatPage = new ChatTabPageView();
+        ITabPageView settingsPage = new SettingsTabPageView();
+
+        this.tabcontrol.addTab(loginPage.getTabPageName(), loginPage.getTabPageIcon(), loginPage.getTabPagePanel(), loginPage.getTabPageTip());
+        this.tabcontrol.addTab(chatPage.getTabPageName(), chatPage.getTabPageIcon(), chatPage.getTabPagePanel(), chatPage.getTabPageTip());
+        this.tabcontrol.addTab(settingsPage.getTabPageName(), settingsPage.getTabPageIcon(), settingsPage.getTabPagePanel(), settingsPage.getTabPageTip());
+    }
     @Override
     public void onPropertyChanged(Object sender, PropertyChangedEventArgs e) {
         System.out.println(sender.toString() + e.getPropertyName());
